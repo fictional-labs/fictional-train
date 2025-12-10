8ae67ea9811ac3e5125d034cd07e7e7caef5fcc5
Yoo Seung-jun <funczun@gmail.com>
2025-12-10 12:47:09 +0900

    fix(user): 예약어 충돌 회피 및 PK 생성 DB 위임

--- backend/src/main/java/com/w/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/w/backend/domain/user/entity/User.java
@@ -1,12 +1,13 @@
 package com.w.backend.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
